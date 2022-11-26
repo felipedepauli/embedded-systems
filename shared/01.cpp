0d3167dd028d8bc82b570d0bfc970fd6ac26faa4
felipedepauli <fcdp83@gmail.com>
2022-11-26 09:23:25 -0300

    ESW_00_01:: adds gitignore and other files

--- shared/01.cpp
+++ shared/01.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
+#include <memory>
 
+class Shared_Memory {
+    private:
+        int *data;
+    
+}
 
-
+Shared_Memory::Shared_Memory() {
+    data = new int[100];
+}
 
 
 int main() {
 
 
-    std::cout << "Hello World!" << std::endl;
+
 
 
     return 0;
